023b169c244a066a33aab0e7612e6f0126eb7e4c
Antonio Martins <anmmartyns@gmail.com>
2021-05-26 18:57:07 -0300

    exercícios 1 à 3 do bloco 4, dia 3

---
+++ 01 - Fundamentos/bloco_5/dia_1/script.js
@@ -0,0 +1,17 @@
+document.getElementById("header-container").style.backgroundColor = "#3cb371";
+
+document.getElementById("container").style.backgroundColor = "#d3d3d3";
+
+document.getElementById("footer-container").style.backgroundColor = "#0d341e";
+
+document.getElementsByClassName("emergency-tasks")[0].style.backgroundColor = "#bc5e57";
+
+document.getElementsByClassName("no-emergency-tasks")[0].style.backgroundColor = "#c5c773";
+
+document.querySelectorAll(".emergency-tasks h3")[0].style.backgroundColor = "#fc0fc0";
+
+document.querySelectorAll(".emergency-tasks h3")[1].style.backgroundColor = "#fc0fc0";
+
+document.querySelectorAll(".no-emergency-tasks h3")[0].style.backgroundColor = "#080200";
+
+document.querySelectorAll(".no-emergency-tasks h3")[1].style.backgroundColor = "#080200";
